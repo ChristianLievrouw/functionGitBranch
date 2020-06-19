16eda203f58b79998a8e3a983feeb95de951454a
ChristianLievrouw <celievrouw1@gmail.com>
2020-06-19 16:21:12 -0500

    added new code to question 2

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 // function multiply(a, b) { //eslint-disable-line
 //   var productNumber = a * b;
-//   var productMessage = 'The product of ' + a + ' * ' + b + ' = ' + productNumber;
+//   var productMessage = 'The product of ' + a + ' and ' + b + ' is ' + productNumber + '.';
 //   return [productNumber, productMessage];
 // }
 
@@ -51,16 +51,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-  var theSum = a + b + c;
-  var theProduct = a * b * c;
-  var sumMessage = a + ' and ' + b + ' and ' + c + ' sum to ' + theSum + '.';
-  var productMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + theProduct + '.';
-  return [theSum, theProduct, sumMessage, productMessage];
-}
+// function sumAndMultiply(a, b, c) { //eslint-disable-line
+//   var theSum = a + b + c;
+//   var theProduct = a * b * c;
+//   var sumMessage = a + ' and ' + b + ' and ' + c + ' sum to ' + theSum + '.';
+//   var productMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + theProduct + '.';
+//   return [theSum, theProduct, sumMessage, productMessage];
+// }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+//testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
